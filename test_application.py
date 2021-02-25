c1b7c3e09ad01139bb59a8d32ee315a5f0552579
Scott Wedge <scott.wedge@gmail.com>
2021-02-24 22:27:02 -0500

    Import 'mocker' as function parameter; substitute 'return_value' in <module>.<function>

--- test_application.py
+++ test_application.py
@@ -7,7 +7,8 @@ from application import *
 
 
 # Functions
-def test_get_os():
+def test_get_os(mocker):  # must import 'mocker' as parameter
+    mocker.patch("application.is_windows", return_value = True) # mock function
     assert get_operating_system() == "Windows"
 
 
@@ -18,3 +19,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# application.py 
+# from https://medium.com/analytics-vidhya/mocking-in-python-with-pytest-mock-part-i-6203c8ad3606
+
+#from time import sleep  
+
+#def is_windows():    
+#    # This sleep could be some complex operation instead
+#    sleep(5)    
+#    return True  
+#
+#def get_operating_system():    
+#    return 'Windows' if is_windows() else 'Linux'
