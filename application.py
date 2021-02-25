2449e8538cbc90b457194bae05307407b3ac7890
Scott Wedge <scott.wedge@gmail.com>
2021-02-24 20:48:16 -0500

    Initial commit

---
+++ application.py
@@ -0,0 +1,11 @@
+# application.py 
+
+from time import sleep  
+
+def is_windows():    
+    # This sleep could be some complex operation instead
+    sleep(5)    
+    return True  
+
+def get_operating_system():    
+    return 'Windows' if is_windows() else 'Linux'
