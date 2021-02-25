c784b9c100fd900e9de28e30de356e5a58838d42
Scott Wedge <scott.wedge@gmail.com>
2021-02-24 21:48:44 -0500

    Program runs with five second delay without mocking

---
+++ test_application.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# test application.py
+
+# Imports
+from application import *
+
+
+# Functions
+def main():
+    result = get_operating_system()
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
